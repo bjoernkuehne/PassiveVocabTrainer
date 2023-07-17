e29dd96d17363bd1c85945fb05ff57ffb30dfe7a
bjoernkuehne <53338803+bjoernkuehne@users.noreply.github.com>
2023-07-17 11:52:24 +0000

    created mock-data and added first state

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
+import { useState } from "react";
 import LearnView from "./components/LearnView";
 import "./styles.css";
+import { createMockVocabSets } from "./data/mock-data";
 
 export default function App() {
+  const [vocabSets, setVocabSets] = useState(createMockVocabSets(5, 10))
 
   return (
     <div className="App">
       <h1>Passive Vocab Trainer</h1>
-      <LearnView />
     </div>
   );
 }
--- src/components/LearnView.tsx
+++ src/components/LearnView.tsx
@@ -1,5 +1,15 @@
-const LearnView = () => {
-    return <div>Learn View</div>
+import IVocabSet from "../interfaces/VocabSet"
+
+interface IProps {
+    vocabSet: IVocabSet[]
+}
+
+const LearnView = (props: IProps) => {
+    return (
+        <div>
+            <h1>Learn View</h1>
+        </div>
+    )
 }
 
 export default LearnView
